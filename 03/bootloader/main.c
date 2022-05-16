538312abae469819ad977bdac58a61535f09d69b
mametarou967 <mametarou967@gmail.com>
2022-05-16 17:00:18 +0900

    03-PA!=VAの処理を追加し、帯域変数への値変更が可能になった

--- 03/bootloader/main.c
+++ 03/bootloader/main.c
@@ -2,17 +2,55 @@
 #include "serial.h"
 #include "lib.h"
 
+volatile int value = 10;
+int value3;
+
+static int init(void)
+{
+	extern int erodata;
+	extern int data_start,edata;
+	extern int bss_start,ebss;
+
+	memcpy(&data_start,&erodata,(long)&edata - (long)&data_start);
+	memset(&bss_start,0,(long)&ebss - (long)&bss_start);
+	
+	serial_init(SERIAL_DEFAULT_DEVICE);
+
+	return 0;
+}
+
 int main(void)
 {
-	serial_init(SERIAL_DEFAULT_DEVICE);
+	init();
+
+	int value2 = 30;
 
 	puts("Hello Kozos\n");
 	
-	putxval(0x10,0);
+	putxval(value,0);
 	puts("\n");
 
-	putxval(0xffff,0);
+	value = 20;
+
+	putxval(value,0);
 	puts("\n");
+
+	putxval(value2,0);
+	puts("\n");
+	
+	value2 = 40;
+
+	putxval(value2,0);
+	puts("\n");
+	
+	putxval(value3,0);
+	puts("\n");
+
+	value3 = 50;
+
+	putxval(value3,0);
+	puts("\n");
+
 
 	while(1)
 		;

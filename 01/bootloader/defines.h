b17920d2b0689a0af3eaf35aa565443dae225699
mametarou967 <mametarou967@gmail.com>
2022-05-16 11:58:29 +0900

    01-とりあえずターゲットボードに書き込めるようになるところまで

---
+++ 01/bootloader/defines.h
@@ -0,0 +1,10 @@
+#ifndef _DEFINES_H_INCLUDED_
+#define _DEFINES_H_INCLUDED_
+
+#define NULL ((void *)0)
+
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned long uint32;
+
+#endif // _DEFINES_H_INCLUDED_

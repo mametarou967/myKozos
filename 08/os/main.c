f4028eccc40650426e0566e07957d4c5de52ec91
mametarou967 <mametarou967@gmail.com>
2022-06-01 23:44:56 +0900

    80実行完了

--- 08/os/main.c
+++ 08/os/main.c
@@ -1,58 +1,21 @@
 #include "defines.h"
-#include "intr.h"
+#include "kozos.h"
 #include "interrupt.h"
-#include "serial.h"
 #include "lib.h"
 
-static void intr(softvec_type_t type,unsigned long sp)
+static int start_threads(int argc,char *argv[])
 {
-	int c;
-	static char buf[32];
-	static int len;
-
-	c = getc();
-
-	if(c != '\n')
-	{
-		buf[len++] = c;
-	}
-	else
-	{
-
-		buf[len++] = '\0';
-		if(strncmp(buf,"echo",4) == 0)
-		{
-			puts(buf + 4);
-			puts("\n");
-		}
-		else
-		{
-			puts("unknown.\n");
-		}
-		puts("> ");
-		len = 0;
-	}
+	kz_run(test08_1_main, "command",0x100,0,NULL);
+	return 0;
 }
 
 int main(void)
 {
 	INTR_DISABLE; // 割込み無効
 
-	puts("go\n");
+	puts("kozos boot succeed!\n");
 
-	// 割込みハンドラの登録
-	softvec_setintr(SOFTVEC_TYPE_SERINTR,intr);
-	// シリアルの割り込み受付の有効化
-	serial_intr_recv_enable(SERIAL_DEFAULT_DEVICE);
-
-	puts("> ");
-
-	INTR_ENABLE;// 割込み有効
-
-	while(1)
-	{
-		asm volatile("sleep");
-	}
+	kz_start(start_threads,"start", 0x100, 0 ,NULL);
 
 	return 0;
 }
